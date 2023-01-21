2ffbb333c56141db069e6161fb26aff7d50a70e3
cpsandeepa <sandeepaamcp@gmail.com>
2023-01-21 21:28:48 +0000

    add poisoning attacks

---
+++ inference-src/__init__.py
@@ -0,0 +1,4 @@
+from .attack_model import AttackModels
+from .data_synthesis import synthesize, synthesize_batch
+from .shadow_model import ShadowModels
+from .shadow_model_old import ShadowModelsOld
